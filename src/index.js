2643904dd3ed077742b85f31636faac7a72439c6
Danil Serobabin <serobabin.danil@gmail.com>
2022-07-31 20:02:30 +0300

    fix modal

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import 'popper.js';
 /* eslint-disable-next-line */
-import { Grid, Accordion, Api } from 'bootstrap';
+import bootstrap from 'bootstrap';
 import app from './init';
 
 app();
